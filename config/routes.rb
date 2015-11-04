7ecee2e2f74deed9e248b679158e71a577222f02
Daniel Mruk <daniel.mruk1@gmail.com>
2015-11-04 17:51:46 -0500

    html edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   
+  # used for stripe
+
+  resources :charges
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
